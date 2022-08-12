fca2f322eab4a7b86fb540c07796cb7181857ff5
AKuchevskaya <alena.kuchevskaya@yandex.ru>
2022-08-12 20:42:51 +0300

    feat: сверстать компонент AboutProject

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -10,10 +10,10 @@ function Main() {
     return (
         <div className="main">
             <img className="main__image-web" src={webWord} alt="Картинка мира" />
-            <h1 className="title style">
+            <h1 className="main__title">
                 Учебный проект студента факультета Веб-разработки.
             </h1>
-            <p className="subtitle">
+            <p className="main__subtitle">
                 Листайте ниже, чтобы узнать больше про этот проект и его создателя.
             </p>
             <button className="main__button">Узнать больше</button>
@@ -22,4 +22,4 @@ function Main() {
     );
 }
 
-export  default Main;
+export default Main;
